7c0b1c222b61ddd1a74a8a76b90d8fae129689c8
OrdinaryWitch <yw.chen.math@gmail.com>
2020-03-15 20:22:35 +0800

    Finished CART docs.

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ df = pd.read_csv('data/balance_scale.csv')
 dt = DecisionTree(df, algorithm='C4.5')
 with open('dt.txt', 'w') as f:
     print(dt, file=f)
+                                                                                                                            
